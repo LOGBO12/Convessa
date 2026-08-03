42b03d770ceb314c3637b78918eab612b6be6f9f
Mirco <mircohouenangnon@gmail.com>
2026-08-03 17:35:25 +0100

    correction

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import fr from './locales/fr.js';
+import fr from './locales/fr.json';
 import en from './locales/en.json';
 import es from './locales/es.json';
 
